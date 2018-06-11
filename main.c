c2da16c74fe86b6322ec45afee132542226641cb
Zimonzk <simon.artner@gmail.com>
2018-06-11 23:31:53 +0200

    add 'multi versions of arraylist functions

--- main.c
+++ main.c
@@ -29,6 +29,12 @@ static inline void copydata(uint16_t unit_size, void* dest, void* source)
     }
 }
 
+static inline void shiftdata(uint64_t shiftwitdh)
+{
+    /*TODO shift elements efficiently*/
+    /*for(uint64_t i = 0; i*/
+}
+
 void arraylist_append(arraylist* list, void* element_ptr)
 {
     list->used_units++;
@@ -44,6 +50,7 @@ void arraylist_append(arraylist* list, void* element_ptr)
 
 void arraylist_instert(arraylist* list, void* element_ptr, uint64_t index)
 {
+    /*TODO shift the old entries up*/
     copydata(list->unit_size, (list->data + (index * list->unit_size)), element_ptr);
 }
 
@@ -67,4 +74,32 @@ void arraylist_replace(arraylist* list, void* element_ptr, uint64_t index)
 void* arraylist_get(arraylist* list, uint64_t index)
 {
     return (void*)(((char*)list->data) + (index * list->unit_size));
+}
+
+void arraylist_append_multi(arraylist* list, void* element0_ptr, uint64_t index, uint64_t ammount)
+{
+    list->used_units += ammount;
+    if(list->used_units > list->allocated_units) {
+        if((list->used_units % list->allocation_block_units) != 0) {
+            list->allocated_units = (list->used_units / list->allocation_block_units) + 1;
+				} else {
+            list->allocated_units = (list->used_units / list->allocation_block_units);
+				}
+        list->data = realloc(list->data, list->allocated_units * list->unit_size);
+        if(list->data == 0) {
+            exit(1);
+        }
+    }
+    copydata(list->unit_size * ammount, (list->data + (index * list->unit_size)), element0_ptr);
+}
+
+void arraylist_insert_multi(arraylist* list, void* element0_ptr, uint64_t index, uint64_t ammount)
+{
+    copydata(list->unit_size * ammount, (list->data + (index * list->unit_size)), element0_ptr);
+}
+
+void arraylist_replace_multi(arraylist* list, void* element0_ptr, uint64_t index, uint64_t ammount)
+{
+    /*TODO shift the old entries up*/	
+    copydata(list->unit_size * ammount, (list->data + (index * list->unit_size)), element0_ptr);
 }
